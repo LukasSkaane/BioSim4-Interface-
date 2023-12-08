de1cbc97c526c303456b4e492615d23c9cbb01cf
SMURFoKING <54553393+SMURFoKING@users.noreply.github.com>
2023-12-08 20:40:47 +1300

    First commit. Program displays a .avi file that shows creatures living in an empty environment. Two GUI elements are implemented, the configuration parameters are shown on the top one, where you can change the values they hold. Beyond what you see there is nothing however, there are still many things to implement:).

---
+++ Scripts/HideGUI.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HideGUI : MonoBehaviour {
+    bool hidden = true, shouldMove = false;
+    float distance = Screen.width / 3, newPosition;
+    public GameObject outerButton;
+
+    public void execute(bool goLeft) {
+        shouldMove = true;
+        if (hidden)
+            newPosition = transform.position.x + distance * (goLeft ? 1 : -1);
+        if (!hidden)
+            newPosition = transform.position.x - distance * (goLeft ? 1 : -1);
+        hidden = !hidden;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (shouldMove) {
+            moveTowardsX(gameObject);
+            if (gameObject.transform.position.x == newPosition) {
+                outerButton.SetActive(hidden);
+                shouldMove = !shouldMove;
+            }
+        }
+
+    }
+
+    void moveTowardsX(GameObject objToMove) {
+        objToMove.transform.position = Vector2.MoveTowards(objToMove.transform.position, new Vector2(newPosition, objToMove.transform.position.y), 3);
+    }
+}
